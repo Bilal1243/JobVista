54b7907b87435652eff62e2093f5db378e982fd9
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-02-28 17:39:42 +0530

    changed port

--- frontend/src/Utils/URL.js
+++ frontend/src/Utils/URL.js
@@ -1,3 +1,3 @@
-export const PROFILE_PATH = 'http://localhost:1001/Profiles/';
-export const POST_IMAGES_PATH = 'http://localhost:1001/Posts/';
-export const RESUME_PATH = "http://localhost:1001/Resume/"
+export const PROFILE_PATH = 'http://localhost:2000/Profiles/';
+export const POST_IMAGES_PATH = 'http://localhost:2000/Posts/';
+export const RESUME_PATH = "http://localhost:2000/Resume/"
