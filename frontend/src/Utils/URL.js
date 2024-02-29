93a5c6decaeb8709cee17b4922a096ad724568e8
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-02-29 18:38:00 +0530

    url frontend

--- frontend/src/Utils/URL.js
+++ frontend/src/Utils/URL.js
@@ -1,3 +1,3 @@
-export const PROFILE_PATH = 'http://3.6.201.194:5000/Profiles/';
-export const POST_IMAGES_PATH = 'http://localhost:5000/Posts/';
-export const RESUME_PATH = "http://localhost:5000/Resume/"
+export const PROFILE_PATH = 'https://muhammedbilal.online:5000/Profiles/';
+export const POST_IMAGES_PATH = 'https://muhammedbilal.online:5000/Posts/';
+export const RESUME_PATH = "https://muhammedbilal.online:5000/Resume/";
