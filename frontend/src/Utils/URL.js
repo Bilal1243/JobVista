3df624e074bf8f4a68bfea4e8cd69e99445b80f6
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-02-29 18:41:40 +0530

    changes urls

--- frontend/src/Utils/URL.js
+++ frontend/src/Utils/URL.js
@@ -1,3 +1,3 @@
-export const PROFILE_PATH = 'https://muhammedbilal.online:5000/Profiles/';
-export const POST_IMAGES_PATH = 'https://muhammedbilal.online:5000/Posts/';
-export const RESUME_PATH = "https://muhammedbilal.online:5000/Resume/";
+export const PROFILE_PATH = 'https://muhammedbilal.online/Profiles/';
+export const POST_IMAGES_PATH = 'https://muhammedbilal.online/Posts/';
+export const RESUME_PATH = "https://muhammedbilal.online/Resume/";
