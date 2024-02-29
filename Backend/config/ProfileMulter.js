2faab0898081a5d7d1bef25b3a9cbf9dee495565
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-02-29 12:43:18 +0530

    multer changes

--- Backend/config/ProfileMulter.js
+++ Backend/config/ProfileMulter.js
@@ -1,19 +1,17 @@
 import multer from 'multer';
 import path from 'path';
 
-
 const storage = multer.diskStorage({
-
-  destination: (req, file, cb) => { cb(null, "Backend/Public/Profiles") },
-
-  filename: (req, file, cb) => { cb( null, file.fieldname + "_" + Date.now() + path.extname(file.originalname) ) }
-  
+  destination: (req, file, cb) => {
+    cb(null, path.resolve("Backend/Public/Profiles"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "_" + Date.now() + "_" + Math.random().toString(36).substr(2, 9) + path.extname(file.originalname));
+  }
 });
-
-
 
 const ProfileUpload = multer({
   storage: storage
 });
 
-export default ProfileUpload
+export default ProfileUpload;
