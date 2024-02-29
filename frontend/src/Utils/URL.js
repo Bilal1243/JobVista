c14614ee03f6a5ee99ec88ee3c73f9199264184a
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-02-29 20:47:58 +0530

    changed url

--- frontend/src/Utils/URL.js
+++ frontend/src/Utils/URL.js
@@ -1,3 +1,3 @@
-export const PROFILE_PATH = 'http://localhost:4000/Profiles/';
-export const POST_IMAGES_PATH = 'http://localhost:4000/Posts/';
-export const RESUME_PATH = "http://localhost:4000/Resume/";
+export const PROFILE_PATH = 'http://123.456.789.012/Profiles/';
+export const POST_IMAGES_PATH = 'http://123.456.789.012/Posts/';
+export const RESUME_PATH = "http://123.456.789.012/Resume/";
