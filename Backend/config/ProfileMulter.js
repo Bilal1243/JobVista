969c05b26e42cb79e286dacea858b8b5e9ba64f7
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-02-29 13:17:07 +0530

    multer chaged

--- Backend/config/ProfileMulter.js
+++ Backend/config/ProfileMulter.js
@@ -1,14 +1,15 @@
 import multer from 'multer';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.resolve(__dirname, '../Public/Profiles'));
   },
   filename: (req, file, cb) => { cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname)) }
-
 });
 
 const ProfileUpload = multer({
