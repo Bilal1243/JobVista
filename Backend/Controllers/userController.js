8d8eaac19115bed706a5597c9f5ef277f20f0ec7
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-03-05 22:32:55 +0530

    changes in usercontrolled

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -118,7 +118,7 @@ const googleRegister = asyncHandler(async (req, res) => {
 
     const userExists = await users.findOne({ email });
     if (userExists.isBlocked) {
-        res.json({status : false})
+        return res.json({status : false})
     }
     else {
         if (userExists) {
